c55a261dc95796a48605801f60604b9d2f080121
Drew DeHaas <drewdehaas@gmail.com>
2024-12-09 14:35:48 -0500

    Give up on .readthedocs.yaml and just update PYTHONPATH in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,8 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sys
+import os
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -20,9 +22,13 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+# Nasty workaround for RTD being annoying to test with. There is probably a better
+# way to do this using .readthedocs.yaml
+thisdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(thisdir, ".."))
